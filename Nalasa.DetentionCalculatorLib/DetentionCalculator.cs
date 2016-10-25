399f6a2bf2d57ec95f7b61b82b1f98937da07a6a
ikunalrai@gmail.com <ikunalrai@gmail.com>
2016-10-25 10:22:51 +0530

    changes

--- Nalasa.DetentionCalculatorLib/DetentionCalculator.cs
+++ Nalasa.DetentionCalculatorLib/DetentionCalculator.cs
@@ -8,16 +8,41 @@ namespace Nalasa.DetentionCalculatorLib
 {
     public class DetentionCalculator:IDetentionCalculator
     {
-        private Dictionary<string, double> _store = new Dictionary<string, double>();
+        public Dictionary<string, double> _store = new Dictionary<string, double>();
+        public Dictionary<string, double> _rulestore = new Dictionary<string, double>();
 
-        public int CalculateDetention(List<OffenceType>,DetentionRule rule,DetentionType det_type)
+        public double CalculateDetention(List<OffenceType> offtypes, DetentionRule rule, DetentionType det_type)
         {
-            throw new NotImplementedException();
+            var det_Period=0.0;
+            foreach (var item in offtypes)
+            {
+                det_Period += item.Period;
+            }
+
+            det_Period = CalculteDiscount(det_Period,rule.DetentionDiscount);
+
+            if (det_Period > 8)
+                throw new Exception("Period greator than 8 Hrs. Parent concerns required");
+            return det_Period;
         }
 
-        public void AddRule()
+        private double CalculteDiscount(double det_Period, int p)
         {
-            throw new NotImplementedException();
+            var s = (p * det_Period) / 100;
+
+            return det_Period - s;
+        }
+
+        public void AddDetentionRule(DetentionRule rule)
+        {
+            var name = rule.RuleName;
+            var period =rule.DetentionDiscount;
+           if (_rulestore.ContainsKey(name))
+            {
+                _rulestore[name] = period;
+            }
+            else
+                _rulestore.Add(name, period);
         }
        
         public void AddOffenceType(OffenceType offenceType)
--- Nalasa.DetentionCalculatorLib/OffenceType.cs
+++ Nalasa.DetentionCalculatorLib/OffenceType.cs
@@ -19,20 +19,6 @@ namespace Nalasa.DetentionCalculatorLib
         public string RuleName { get; set; }
         public int DetentionDiscount { get; set; }
         
-        public int DetentionPeriod { get; set; }
-
-
-        private Dictionary<string, int> _store = new Dictionary<string, int>();
-        public void AddDetentionRule(string name, int period)
-        {
-            if (_store.ContainsKey(name))
-            {
-                _store[name] = period;
-            }
-            else
-                _store.Add(name, period);
-        }
-
     }
 
     public enum DetentionType
